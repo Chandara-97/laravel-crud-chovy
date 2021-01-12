7ad2c93d807db9bcdaea6b64a2f86ca230eb153d
NaryLy <naryaimeting@gmail.com>
2021-01-12 12:39:11 +0700

    Powered By Nary

--- resources/views/UserInterface/contactus.blade.php
+++ resources/views/UserInterface/contactus.blade.php
@@ -23,7 +23,7 @@
              <div class="col-2">
                 <img class="rounded-circle" style="width: 18rem;" src="https://z-p3-scontent.fpnh18-3.fna.fbcdn.net/v/t1.0-9/118175273_2422841798016454_7512749369154338776_n.jpg?_nc_cat=103&amp;ccb=2&amp;_nc_sid=174925&amp;_nc_eui2=AeF6rF02hJorJu9lQNiPlRaF_M92PINQepb8z3Y8g1B6loS-vViW37WbEedOTrr7N9T_HehpjAYTBV4-6XF7rAnL&amp;_nc_ohc=fA1nR_ZTXPQAX98RSf1&amp;_nc_ht=z-p3-scontent.fpnh18-3.fna&amp;oh=c961fa0d514c99860d15e698f1294f90&amp;oe=5FFB405E" alt="Leav Chan Dara">
                 <div class="text-center">
-                  <h2>Leav Chan Dara</h2>
+                  <h5>Leav Chan Dara</h5>
                 </div>
                 <div class="text-center">
                    <a href="https://web.facebook.com/leav.chandara"><i class="fab fa-facebook fa-lg" aria-hidden="true"></i></a>&nbsp;
@@ -34,7 +34,7 @@
              <div class="col-2">
                 <img class="rounded-circle" style="width: 18rem;" src="https://z-p3-scontent.fpnh18-1.fna.fbcdn.net/v/t1.0-9/127278990_1271592736537593_965136852605287300_n.jpg?_nc_cat=108&amp;ccb=2&amp;_nc_sid=09cbfe&amp;_nc_eui2=AeFRHUrAQHgVMUj0_FwQikD_b-7L_MUlAyVv7sv8xSUDJa0fgxogJJkj3foFPhH0a-oHzhOoYsrpWqCBXO6ZXkpS&amp;_nc_ohc=j2hiuXgAXjYAX_nzADE&amp;_nc_ht=z-p3-scontent.fpnh18-1.fna&amp;oh=6c983a3d7b43d690bb531ca51d448a01&amp;oe=5FFAD06B" alt="ly nary">
                 <div class="text-center">
-                  <h2>Ly Nary</h2>
+                  <h5>Ly Nary</h5>
                 </div>
                 <div class="text-center">
                    <a href="https://web.facebook.com/profile.php?id=100010606367727"><i class="fab fa-facebook fa-lg" aria-hidden="true"></i></a>&nbsp;
@@ -46,11 +46,11 @@
        </div> <br>
        <div class="row">
           <div class="col-4"></div>
-          <div class="col-4"><span class="btn btn-primary text-white text-center"><h2>OR</h2></span></div>
+
           <div class="col-4"></div>
        </div><br><br><br>
          <div class="container">
-              <form action="{{route('contact.store')}}">
+              <form action="">
                   <div class="form-group">
                           <div>
                               <label class="move-up" for="name"> Full Name </label>
